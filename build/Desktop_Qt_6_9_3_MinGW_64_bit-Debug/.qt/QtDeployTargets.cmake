3fcba2d3f55a743b3682803ff6ea6c5406ef12ca
Tanvirahmed <tanvirchy269@gmail.com>
2026-02-07 23:04:36 +0600

    chore: update gitignore to exclude build and creator files

--- build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appdashboard_2_FILE D:/Projects/Qt Projects/dashboard_2/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/appdashboard_2.exe)
-set(__QT_DEPLOY_TARGET_appdashboard_2_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appdashboard_2_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Network.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
